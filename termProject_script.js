09ea7019a8985af742d716976eb90d3eba2c5de5
positiveWand <49838556+positiveWand@users.noreply.github.com>
2019-12-15 02:52:31 +0900

    Update termProject_script.js

--- termProject_script.js
+++ termProject_script.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
-    $(".selectPositionMessage").hide();
+    $("#selectPositionMessage").hide();
     
-    $(".addTripButton").on("click", function(){
+    $("#addTripButton").on("click", function(){
         /*
         <화면 전환>
         -지도의 초기 위치(여행의 시작 위치)를 설정하도록함
@@ -11,7 +11,7 @@ $(document).ready(function() {
         //지도 초기 위치 설정 화면
         //content내용 전환
         $("#tripList").hide();
-        $("selectPositionMessage").show();
+        $("#selectPositionMessage").show();
         //control버튼들 비활성화
         $("#selectAlign").attr("disabled", true);
         $("#alignButton").attr("disabled", true);
