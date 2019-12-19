f5d68cf0df75885a03b94d97399459c2ad6c3320
positiveWand <49838556+positiveWand@users.noreply.github.com>
2019-12-19 15:35:06 +0900

    Update savePoint.php

--- savePoint.php
+++ savePoint.php
@@ -26,8 +26,9 @@ $tripSummaryJSON = file_get_contents($fileName);
 $tripSummaryArray = json_decode($tripSummaryJSON, true);
 
 array_push($tripSummaryArray["pointsOrder"], $pointName);
+var_dump($tripSummaryArray);
 
-file_put_contents($fileName, json_encode($tripSummaryArray, JSON_UNESCAPED_UNICODE))
+//file_put_contents($fileName, json_encode($tripSummaryArray, JSON_UNESCAPED_UNICODE))
 
 
 
