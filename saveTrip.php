3657d7309a1b6cff002c81cae9e4a5735682adcd
positiveWand <49838556+positiveWand@users.noreply.github.com>
2019-12-19 17:39:27 +0900

    Update saveTrip.php

--- saveTrip.php
+++ saveTrip.php
@@ -2,7 +2,7 @@
 error_reporting(E_ALL);
 
 ini_set("display_errors", 1);
-$newTrip = $_POST["newTrip"]
+$newTrip = $_POST["newTrip"];
 $tripName = $_POST["tripName"];
 $tripStartDate = $_POST["tripStartDate"];
 $tripEndDate = $_POST["tripEndDate"];
